a87a3746b68864760b06dd9fafbee48e23ccc66d
SanchezsX -dev~ <113286578+SanchezsX@users.noreply.github.com>
2024-05-29 13:49:40 +0200

    Add src/supabase.ts to .gitignore

--- src/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const URL = 'https://algbhmfsgderfplxubyo.supabase.co'
-const KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsZ2JobWZzZ2RlcmZwbHh1YnlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIwNjM5NjUsImV4cCI6MjAyNzYzOTk2NX0.j4N5v9_FkTvQxDlPSw0nruzjw1f7fc01AOZNPFXpE-o'
-
-export const supabase = createClient(URL, KEY!);
